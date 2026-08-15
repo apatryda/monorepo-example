0da8d339e637f435eed269c5e9a7da01d40ae863
Artur Nerkowski <a.apatryda@gmail.com>
2026-08-16 01:07:40 +0200

    build(vue-app): configure Vitest and eslint-plugin-vue-pug integration

--- packages/vue-app/eslint.config.js
+++ packages/vue-app/eslint.config.js
@@ -1,3 +1,30 @@
 import vuetify from 'eslint-config-vuetify'
+// Importing the eslint-plugin-vue-pug package itself eagerly loads its whole
+// `lib/configs/*` tree, which resolves `vue-eslint-parser` in a way that Yarn PnP
+// rejects. Importing just the two rules we need sidesteps that entirely.
+import noParsingError from 'eslint-plugin-vue-pug/lib/rules/no-parsing-error.js'
+import noPugControlFlow from 'eslint-plugin-vue-pug/lib/rules/no-pug-control-flow.js'
 
-export default vuetify()
+export default vuetify().then(config => [
+  ...config,
+  {
+    files: ['**/*.vue'],
+    plugins: {
+      'vue-pug': {
+        rules: {
+          'no-parsing-error': noParsingError,
+          'no-pug-control-flow': noPugControlFlow,
+        },
+      },
+    },
+    languageOptions: {
+      parserOptions: {
+        templateTokenizer: { pug: 'vue-eslint-parser-template-tokenizer-pug' },
+      },
+    },
+    rules: {
+      'vue-pug/no-parsing-error': 'error',
+      'vue-pug/no-pug-control-flow': 'warn',
+    },
+  },
+])
---
+++ packages/vue-app/src/test/setup.ts
@@ -0,0 +1,25 @@
+import { config } from '@vue/test-utils'
+import vuetify from '@/plugins/vuetify'
+
+if (!window.matchMedia) {
+  window.matchMedia = (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }) as unknown as MediaQueryList
+}
+
+if (!window.ResizeObserver) {
+  window.ResizeObserver = class {
+    observe () {}
+    unobserve () {}
+    disconnect () {}
+  }
+}
+
+config.global.plugins.push(vuetify)
